62c73df9fb903bff56ff2b7ee672abdf51dabc51
shrutipadole <shrutipadole339@gmail.com>
2023-03-07 19:19:38 -0500

    ddd

--- helpers/data_aggregator.py
+++ helpers/data_aggregator.py
@@ -1,7 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
-df = pd.read_csv("data.csv")
+# df = pd.read_csv("data.csv")
 
 
 
@@ -12,8 +12,10 @@ with open(path_to_list_of_files, "r") as f:
     list_of_main_files = f.readlines()
 
 onlyfiles = [f for f in listdir(dump_path) if isfile(join(dump_path, f))]
-
+main_list = []
 for each_file in list_of_main_files:
-    list_of_sub_files = [pd.read_csv(fle+".csv") for fle in onlyfiles if fle.startswith(each_file)]
+    list_of_sub_files = [pd.read_csv(dump_path+fle, on_bad_lines='skip') for fle in onlyfiles if fle.startswith(each_file)]
+    main_list.append(list_of_sub_files)
+    
     consolidated_df = pd.concat(list_of_sub_files)
-    consolidated_df.to_csv("../data/clean_dataset/"+each_file+".csv")
+    consolidated_df.to_csv("../data/clean_dataset/"+each_file+".tsv",sep = '\t', index=False)
